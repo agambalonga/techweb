396280519395a171bf5c7a0d92d6e69f3ec82d79
agambalonga <af13842@enel.com>
2023-10-14 09:11:06 +0200

    setup upload profile picture

--- events-booking/model/User.js
+++ events-booking/model/User.js
@@ -30,7 +30,10 @@ const userSchema = new mongoose.Schema({
     },
     google_id : {
         type : String
-    }
+    },
+    profile_picture_URL : {
+        type : String
+    },
  });
 
  userSchema.pre(['save', 'update'], async function(next) {
