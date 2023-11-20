b11e4b936d01107373b42910c2bc83d38af9fbd0
agambalonga <af13842@enel.com>
2023-11-20 22:35:30 +0100

    develop open event _wip

--- events-booking/model/Event.js
+++ events-booking/model/Event.js
@@ -39,6 +39,27 @@ const eventSchema = new mongoose.Schema({
     }
 });
 
+eventSchema.methods.formatDateTime = function(inputDate) {
+    const date = new Date(inputDate);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+eventSchema.methods.formatDate = function(inputDate) {
+    const date = new Date(inputDate);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
 const Event = mongoose.model('events', eventSchema)
 
 module.exports = Event;
