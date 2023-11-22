0d01e90c5e44e07d312140524e12eb135a5fd929
FabioCosentUni <fabio.cosentino001@studenti.uniparthenope.it>
2023-11-22 17:25:43 +0100

    Row cart added

--- events-booking/controller/cartController.js
+++ events-booking/controller/cartController.js
@@ -40,10 +40,10 @@ module.exports.add_to_cart = async (req, res) => {
             }
         }
 
-
+ 
         console.log(req.session.cart);
 
-        res.status(200).json({success: true, message: 'Event added to cart', qty_added: req.body.qty, cart: req.session.cart});
+        res.status(200).json({success: true, message: 'Event added to cart', qty_added: req.body.qty, cart: req.session.cart, new_item: newItem});
         
 
     } else {
